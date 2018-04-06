b9330895ab7079594361f053e4ea82fdb8cf6c69
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-04-06 12:14:17 -0300

    Add files via upload

--- facu clase 3/genericas/funciones.c
+++ facu clase 3/genericas/funciones.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 #include "funciones.h"
 
-int pedirentero (char[], int, int);
-int validarentero (char[],int,int,int);
-
-int main()
-{
-    int numero1;
-    int numero2;
-
-    numero1=pedirentero ("numero1: ",1,9999999);
-    numero2=pedirentero ("numero2: ",1,9999999);
-
-    printf("el numero ingresado es: %d", numero1);
-    printf("\nel numero ingresado es: %d", numero2);
-}
-
 int pedirentero (char mensaje[], int min, int max)
 {
     int numero;
@@ -38,27 +23,12 @@ int validarentero (char mensaje[],int dato,int min,int max)
     }
 }
 
-float pedirflotante (char[], float, float);
-float validarflotante (char[],float,float,float);
-
-float main()
-{
-    float numero1;
-    float numero2;
-
-    numero1=pedirflotante ("numero1: ",1,9999999);
-    numero2=pedirflotante ("numero2: ",1,9999999);
-
-    printf("el numero ingresado es: %.2f", numero1);
-    printf("\nel numero ingresado es: %.2f", numero2);
-}
-
 float pedirflotante (char mensaje[], float min, float max)
 {
     float numero;
 
     printf("ingrese %s", mensaje);
-    scanf("%.2f", &numero);
+    scanf("%d", &numero);
 
     numero=validarflotante(mensaje,numero,min,max);
 
@@ -70,6 +40,6 @@ float validarflotante (char mensaje[],float dato,float min,float max)
      while (dato<min || dato>max)
     {
         printf("error reingrese %s", mensaje);
-        scanf("%.2f", &dato);
+        scanf("%d", &dato);
     }
 }
---
+++ facu clase 3/genericas/funciones.h
@@ -0,0 +1,8 @@
+
+int pedirentero (char[], int, int);
+
+int validarentero (char[],int,int,int);
+
+float pedirflotante (char[], float, float);
+
+float validarflotante (char[],float,float,float);
