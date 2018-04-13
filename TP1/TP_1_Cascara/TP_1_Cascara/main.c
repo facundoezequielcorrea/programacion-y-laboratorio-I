1d7b30f6341cb8ea7ab9f93efa46d4061e9763ec
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-04-13 02:28:37 -0300

    Add files via upload

--- TP1/TP_1_Cascara/TP_1_Cascara/main.c
+++ TP1/TP_1_Cascara/TP_1_Cascara/main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "funciones.h"
-
-float pedirflotante (char[]);
-float sumar (float, float);
-float restar (float, float);
-float multiplicar (float, float);
-float dividir (float, float);
-float factorial (float);
-float calculartodaslasoperaciones (float, float);
+#include "funciones.h"
 
 int main()
 {
@@ -20,7 +12,7 @@ int main()
     float resta;
     float producto;
     float division;
-    float factoria;
+    int factoria;
 
     while(seguir=='s')
     {
@@ -36,7 +28,6 @@ int main()
 
         scanf("%d",&opcion);
         fflush( stdin );
-        //opcion= getche();
 
         switch(opcion)
         {
@@ -68,127 +59,26 @@ int main()
                 break;
             case 7:
                 factoria=factorial (numero1);
-                printf("el factorial es: %f\n", factoria);
+                validarfactorial(numero1);
+                printf("el factorial es: %d\n", factoria);
                 break;
             case 8:
                 suma=sumar(numero1, numero2);
                 resta=restar(numero1, numero2);
                 division=dividir(numero1, numero2);
                 producto=multiplicar(numero1, numero2);
+                factoria=factorial (numero1);
+                validarfactorial(numero1);
                 printf("el resultado de la suma es: %.2f\n", suma);
                 printf("el resultado de la resta es: %.2f\n", resta);
                 printf("el resultado de la division es: %.2f\n", division);
                 printf("el resultado de la multiplicacion es: %.2f\n", producto);
-                printf("el factorial es: %d", factoria);
+                printf("el factorial es: %d\n", factoria);
                 break;
             case 9:
                 seguir = 'n';
                 break;
         }
-
     }
     return 0;
-}
-
-
-float pedirflotante (char mensaje[])
-{
-    float numero;
-
-    printf("ingrese %s", mensaje);
-    scanf("%f", &numero);
-
-    return numero;
-}
-
-float sumar (float numero1, float numero2)
-{
-    float suma;
-
-    suma=numero1+numero2;
-
-    return suma;
-}
-
-float restar (float numero1, float numero2)
-{
-    float resta;
-
-    resta=numero1-numero2;
-
-    return resta;
-}
-
-float multiplicar (float numero1, float numero2)
-{
-    float producto;
-
-    producto=numero1*numero2;
-
-    return producto;
-}
-
-float dividir (float numero1, float numero2)
-{
-    float division;
-    if (numero2==0)
-    {
-        printf("Error");
-    }else
-    {
-        division=numero1/numero2;
-    }
-    return division;
-}
-
-float factorial (float numero1)
-{
-    int i;
-    int resp=1;
-    if (numero1<=0)
-    {
-        printf("Error");
-    }
-    else
-    {
-        for (i=numero1; i>=1; i--)
-        {
-            resp=resp*i;
-        }
-    }
-    return resp;
-}
-
-float calculartodaslasoperaciones (float numero1, float numero2)
-{
-    float suma;
-    float resta;
-    float producto;
-    float division;
-    int i;
-    int resp=1;
-
-    suma=numero1+numero2;
-    resta=numero1-numero2;
-    producto=numero1*numero2;
-
-    if (numero2==0)
-    {
-        printf("Error");
-    }else
-    {
-        division=numero1/numero2;
-    }
-
-    if (numero1<0)
-    {
-        printf("Error");
-    }else
-    {
-        for (i=numero1; i>=1; i--)
-        {
-            resp=resp*i;
-        }
-    }
-    return suma, resta, producto, division, resp;
 }
